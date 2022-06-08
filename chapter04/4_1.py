595e622bdbb8763ffaf705607979230ca2eaf3d9
lizhipeng1021 <lizhipeng1021@163.com>
2022-06-08 21:04:35 +0800

    add 4_1.py

--- chapter04/4_1.py
+++ chapter04/4_1.py
@@ -19,5 +19,26 @@ class Duck:
         print('i am a duck')
 
 
-animal = Cat
-animal().say()
+class Company:
+    def __init__(self, employee_list):
+        self.emplyee = employee_list
+
+    def __getitem__(self, item):
+        return self.emplyee[item]
+
+
+company = Company(['green', 'blue'])
+
+animal_list = [Cat, Dog, Duck]
+for animal in animal_list:
+    animal().say()
+
+a = ['bobby1', 'bobby2']
+b = ['bobby2', 'bobby']
+name_tuple = ['bobby3', 'bobby4']
+name_set = set()
+name_set.add('bobby5')
+name_set.add('bobby6')
+a.extend(name_set)
+a.extend(company)
+print(a)
